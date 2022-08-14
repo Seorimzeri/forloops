5ffda34562d46f9aee4c6cfa82f9f849fdcdaf87
Seorimzeri <107941461+Seorimzeri@users.noreply.github.com>
2022-08-14 14:16:23 -0400

    Add files via upload

---
+++ forloop.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class forloop {
+
+	public static void main(String[] args) {
+		   Scanner scan = new Scanner(System.in);
+		   
+		   for(int x = 10; x >=0; x--) {
+			   System.out.println(x);
+		   }
+		   
+		   for(int y = 1; y <= 10; y++) {
+			  System.out.println(y);
+		   		}
+			}
+
+		}
+	
+
